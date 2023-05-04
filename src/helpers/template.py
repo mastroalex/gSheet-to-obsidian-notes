e92700b9624c9d5c8b8f9739f5ff411c197a30d7
Alessandro Mastrofini <alessandro.mastrofini1@gmail.com>
2023-05-04 07:20:49 +0200

    update 2023

--- src/helpers/template.py
+++ src/helpers/template.py
@@ -26,9 +26,9 @@ def chartText(exType,color,date):
     "```dataviewjs  \n"
     f"let EX=\"{exType}\"; \n"
     "dv.header(3,EX);"
-    f"const maxvalue=dv.array(dv.pages('\"Workout\"').where(p=>p.exercisename && p.exerciseName.path==EX && p.dateNoLink<dv.date(\"{date}\")+1 ).max );  \n"
-    f"const meanvalue=dv.array(dv.pages('\"Workout\"').where(p=>p.exercisename && p.exerciseName.path==EX && p.dateNoLink<dv.date(\"{date}\")+1).mean );  \n"
-    f"const mylabel=dv.array(dv.pages('\"Workout\"').where(p=>p.exercisename && p.exerciseName.path==EX && p.dateNoLink<dv.date(\"{date}\")+1).dateNoLink);  \n"
+    f"const maxvalue=dv.array(dv.pages('\"Workout\"').where(p=>p.exercisename && p.exerciseName.path==EX && p.dateNoLink<dv.date(\"{date}\")+1 && p.dateNoLink>dv.date(\"2022-12-30\")+1  ).max );  \n"
+    f"const meanvalue=dv.array(dv.pages('\"Workout\"').where(p=>p.exercisename && p.exerciseName.path==EX && p.dateNoLink<dv.date(\"{date}\")+1 && p.dateNoLink>dv.date(\"2022-12-30\")+1 ).mean );  \n"
+    f"const mylabel=dv.array(dv.pages('\"Workout\"').where(p=>p.exercisename && p.exerciseName.path==EX && p.dateNoLink<dv.date(\"{date}\")+1 && p.dateNoLink>dv.date(\"2022-12-30\")+1 ).dateNoLink);  \n"
     "const chartData = {    \n"
     "type: 'line',    \n"
     "data: {    \n"
@@ -55,25 +55,25 @@ def chartText(exType,color,date):
 
 def fileHeader(date,exerciseType,SxR,reps,weigth):
     header=("---\n"
-    "type::gymExercise \n")
-    datestring="date::[["+str(date)+"]] \n dateNoLink::"+str(date)+" \n"
-    exerciseName="exerciseName::[["+str(exerciseType)+"]] \n"
-    SxR_string="SxR::"+str(SxR)+" \n"
-    S1="S1::"+str(reps[0])+" \n"
-    W1="W1::"+str(weigth[0])+" \n"
-    S2="S2::"+str(reps[1])+" \n"
-    W2="W2::"+str(weigth[1])+" \n"
-    S3="S3::"+str(reps[2])+" \n"
-    W3="W3::"+str(weigth[2])+" \n"
-    S4="S4::"+str(reps[3])+" \n"
-    W4="W4::"+str(weigth[3])+" \n"
-    S5="S5::"+str(reps[4])+" \n"
-    W5="W5::"+str(weigth[4])+"\n"
+    "type:: gymExercise \n")
+    datestring="date:: [["+str(date)+"]] \n dateNoLink::"+str(date)+" \n"
+    exerciseName="exerciseName:: [["+str(exerciseType)+"]] \n"
+    SxR_string="SxR:: "+str(SxR)+" \n"
+    S1="S1:: "+str(reps[0])+" \n"
+    W1="W1:: "+str(weigth[0])+" \n"
+    S2="S2:: "+str(reps[1])+" \n"
+    W2="W2:: "+str(weigth[1])+" \n"
+    S3="S3:: "+str(reps[2])+" \n"
+    W3="W3:: "+str(weigth[2])+" \n"
+    S4="S4:: "+str(reps[3])+" \n"
+    W4="W4:: "+str(weigth[3])+" \n"
+    S5="S5:: "+str(reps[4])+" \n"
+    W5="W5:: "+str(weigth[4])+"\n"
     weigthNum=[float(x.replace(',', '.')) for x in weigth]
-    std="std::"+str(np.nanstd(weigthNum).round(decimals=2))+"\n"
-    maxWeigth="max::"+str(np.nanmax(weigthNum))+"\n"
-    minWeigth="min::"+str(np.nanmin(weigthNum))+"\n"
-    meanWeigth="mean::"+str(np.nanmean(weigthNum).round(decimals=2))+"\n"
+    std="std:: "+str(np.nanstd(weigthNum).round(decimals=2))+"\n"
+    maxWeigth="max:: "+str(np.nanmax(weigthNum))+"\n"
+    minWeigth="min:: "+str(np.nanmin(weigthNum))+"\n"
+    meanWeigth="mean:: "+str(np.nanmean(weigthNum).round(decimals=2))+"\n"
     header=header+datestring+exerciseName+SxR_string+S1+W1+S2+W2+S3+W3+S4+W4+S5+W5+maxWeigth+minWeigth+meanWeigth+std
     header=header+'---\n'
     return header
@@ -82,9 +82,9 @@ def fileHeader(date,exerciseType,SxR,reps,weigth):
 
 def createWorkout(date,dayType,exerciseType,SxR,reps,weigth):
     header=("---\n"
-    "type::workout \n")
-    datestring="date::[["+str(date)+"]] \ndateNoLink::"+str(date)+" \n"
-    dayTypeString="dayType::"+str(dayType)+" \n"
+    "type:: workout \n")
+    datestring="date:: [["+str(date)+"]] \ndateNoLink::"+str(date)+" \n"
+    dayTypeString="dayType:: "+str(dayType)+" \n"
     header=header+datestring+dayTypeString+"cal:: \n"
     header=header+'---\n'
     text=(
